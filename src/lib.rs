14dcdfe92890f9781705fcf70429a51c279ff932
Adrian Benavides <adri.benavides312@gmail.com>
2020-09-25 17:24:12 +0200

    move test to tests directory and add prelude mod

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 
 //! Lapin support for the r2d2 connection pool.
 
+#[allow(missing_docs)]
+pub mod prelude;
+
 use futures_executor::block_on;
 use lapin::protocol::{AMQPError, AMQPErrorKind, AMQPHardError};
 use lapin::types::ShortString;
@@ -13,25 +16,22 @@ use lapin::{Connection, ConnectionProperties, ConnectionState, Error};
 /// # Example
 /// ```no_run
 /// use lapin::ConnectionProperties;
-/// use r2d2_lapin::LapinConnectionManager;
+/// use r2d2_lapin::prelude::*;
 /// use std::thread;
 ///
-/// fn main() {
-///     
 /// let manager = LapinConnectionManager::new("amqp://guest:guest@127.0.0.1:5672//", &ConnectionProperties::default());
-///     let pool = r2d2::Pool::builder()
-///         .max_size(15)
-///         .build(manager)
-///         .unwrap();
+/// let pool = r2d2::Pool::builder()
+///     .max_size(15)
+///     .build(manager)
+///     .unwrap();
 ///
-///     for _ in 0..20 {
-///         let pool = pool.clone();
-///         thread::spawn(move || {
-///             let conn = pool.get().unwrap();
-///             // use the connection
-///             // it will be returned to the pool when it falls out of scope.
-///         });
-///     }
+/// for _ in 0..20 {
+///     let pool = pool.clone();
+///     thread::spawn(move || {
+///         let conn = pool.get().unwrap();
+///         // use the connection
+///         // it will be returned to the pool when it falls out of scope.
+///     });
 /// }
 /// ```
 #[derive(Debug)]
@@ -46,9 +46,7 @@ impl LapinConnectionManager {
     ///
     /// # Example
     /// ```
-    /// use lapin::ConnectionProperties;
-    /// use r2d2_lapin::LapinConnectionManager;
-    /// let manager = LapinConnectionManager::new("amqp://guest:guest@127.0.0.1:5672//", &ConnectionProperties::default());
+    /// let manager = r2d2_lapin::LapinConnectionManager::new("amqp://guest:guest@127.0.0.1:5672//", &lapin::ConnectionProperties::default());
     /// ```
     pub fn new(amqp_address: &str, conn_properties: &ConnectionProperties) -> Self {
         Self {
@@ -85,37 +83,5 @@ impl r2d2::ManageConnection for LapinConnectionManager {
     fn has_broken(&self, conn: &mut Self::Connection) -> bool {
         let broken_states = vec![ConnectionState::Closed, ConnectionState::Error];
         broken_states.contains(&conn.status().state())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::LapinConnectionManager;
-    use lapin::ConnectionProperties;
-    use r2d2::Pool;
-    use std::sync::Arc;
-    use tokio_amqp::LapinTokioExt;
-
-    lazy_static::lazy_static! {
-        static ref AMQP_URL: String = {
-            dotenv::dotenv().ok();
-            std::env::var("TEST_AMQP_URL").unwrap_or_else(|_| "amqp://guest:guest@127.0.0.1:5672//".to_string())
-        };
-    }
-
-    #[tokio::test]
-    async fn can_connect() {
-        let manager = LapinConnectionManager::new(&AMQP_URL, &ConnectionProperties::default().with_tokio());
-        let pool = Arc::new(Pool::builder().max_size(2).test_on_check_out(true).build(manager).unwrap());
-        let n_tasks = 100;
-        for i in 0..n_tasks {
-            let pool = pool.clone();
-            tokio::spawn(async move {
-                let delay_ms = i - n_tasks;
-                tokio::time::delay_for(tokio::time::Duration::from_millis(delay_ms as u64)).await;
-                let conn = pool.get().expect("Should get connection");
-                conn.create_channel().await.expect("Should create lapin channel");
-            });
-        }
     }
 }
---
+++ src/prelude.rs
@@ -0,0 +1,2 @@
+pub use crate::LapinConnectionManager;
+pub use r2d2;
